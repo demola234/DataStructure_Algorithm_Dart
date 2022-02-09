c928b4e5a991ffec5edf4ed5ee46dad56836fb79
Kolawole Ademola <64567320+demola234@users.noreply.github.com>
2022-02-09 15:14:33 +0100

    🤠Day1 of DSA Dart

---
+++ Stacks.dart
@@ -0,0 +1,38 @@
+import 'dart:collection';
+
+class Stack<E> {
+  final _list = <E>[];
+
+  void push(E value) => _list.add(value);
+
+  E pop() => _list.removeLast();
+
+  E get peek => _list.last;
+
+  bool get isEmpty => _list.isEmpty;
+  bool get isNotEmpty => _list.isNotEmpty;
+
+  @override
+  String toString() => _list.toString();
+}
+
+void main() {
+  var letters = Stack<String>();
+
+  var words = "CarRace";
+  var rwords = "";
+
+  for (var i = 0; i < words.length; i++) {
+    letters.push(words[i]);
+  }
+
+  for (var i = 0; i < words.length; i++) {
+    letters.pop();
+  }
+
+  if (rwords == words) {
+    print(words + "This is probagated");
+  } else {
+    print(words + " This is not probagated");
+  }
+}
